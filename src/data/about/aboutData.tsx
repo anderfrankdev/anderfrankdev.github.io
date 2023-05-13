da0debdd5d5ff5b4855f88f43479020e9fe14b1d
Ander Carrasco <anderfrankdev@gmail.com>
2023-05-13 19:20:48 -0400

    image added to about page

---
+++ src/data/about/aboutData.tsx
@@ -0,0 +1,14 @@
+export const introduccionES = [
+	"Soy un desarrollador apasionado por resolver problemas y ayudar a otras personas a lograr sus objetivos. Actualmente, estoy cursando estudios en la <a href='http://www.ucv.ve/' target='_blank'>Universidad Central de Venezuela</a> en la Licenciatura en Ciencias Estadísticas y en la <a href='https://es.wikipedia.org/wiki/Universidad_Nacional_Abierta' target='_blank' >Universidad Nacional Abierta </a> en la Licenciatura en Matemáticas.",
+  	"Desde que comencé a programar a los 20 años, he estado trabajando en proyectos personales de código abierto que me han permitido desarrollar mis habilidades en programación y aprender nuevas tecnologías. Me apasiona encontrar soluciones creativas y eficientes a los problemas, y siempre estoy buscando formas de utilizar la tecnología para mejorar la vida de las personas y proteger nuestro planeta.",
+  	"Mis intereses son variados y van desde la matemática y la física, hasta el ajedrez y el fútbol. Me encanta la naturaleza y la ecología, y siempre estoy buscando formas de utilizar la tecnología para ayudar a proteger nuestro planeta.",
+  	"En mi tiempo libre, disfruto pasar tiempo con mi familia, y también me gusta salir a caminar y explorar la naturaleza. Como desarrollador, me enfoco en aprender continuamente y mejorar mis habilidades, y estoy emocionado por las oportunidades que el futuro pueda traer."
+]
+
+export const introductionEn = [
+	"I am a developer passionate about solving problems and helping other people achieve their goals. Currently, I am studying at the <a href='http://www.ucv.ve/' target='_blank'>Universidad Central de Venezuela</a> for a degree in Statistical Sciences and at the <a href='https://es.wikipedia.org/wiki/Universidad_Nacional_Abierta' target='_blank' >Universidad Nacional Abierta </a> for a degree in Mathematics.",
+  	"Since I started programming at the age of 20, I have been working on personal open source projects that have allowed me to develop my programming skills and learn new technologies. I'm passionate about finding creative and efficient solutions to problems, and I'm always looking for ways to use technology to improve people's lives and protect our planet.",
+  	"My interests are varied and range from mathematics and physics, to chess and soccer. I love nature and ecology, and I'm always looking for ways to use technology to help protect our planet.",
+  	"In my free time, I enjoy spending time with my family, and I also like to go hiking and explore nature. As a developer, I focus on continually learning and improving my skills, and I'm excited for the opportunities the future may bring."
+]
+
--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -3,7 +3,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import { LanContext, ThemeContext } from "../layout";
 import css from "./about.css?inline";
 import { knowledges } from "~/data/workUi";
-
+import { introduccionES, introductionEn } from "~/data/about/aboutData";
 export default component$(() => {
   const theme = useContext(ThemeContext);
   const lan = useContext(LanContext);
@@ -17,16 +17,21 @@ export default component$(() => {
   return (
     <article
       id=""
-      class={"w-screen min-h-screen pt-14 px-24"}
+      class={"w-screen flex min-h-screen pt-14 px-24 items-center"}
     >
-    	<h1 class={"text-6xl font-bold my-8"}>Acerca de mi</h1>
-    	<p class={"text-xl my-4"}>Soy un desarrollador apasionado por resolver problemas y ayudar a otras personas a lograr sus objetivos. Actualmente, estoy cursando estudios en la Universidad Central de Venezuela en la Licenciatura en Ciencias Estadísticas y en la Universidad Nacional Abierta en la Licenciatura en Matemáticas.</p>
-
-		<p class={"text-xl my-4"}>Desde que comencé a programar a los 20 años, he estado trabajando en proyectos personales de código abierto que me han permitido desarrollar mis habilidades en programación y aprender nuevas tecnologías. Me apasiona encontrar soluciones creativas y eficientes a los problemas, y siempre estoy buscando formas de utilizar la tecnología para mejorar la vida de las personas y proteger nuestro planeta.</p>
-
-		<p class={"text-xl my-4"}>Mis intereses son variados y van desde la matemática y la física, hasta el ajedrez y el fútbol. Me encanta la naturaleza y la ecología, y siempre estoy buscando formas de utilizar la tecnología para ayudar a proteger nuestro planeta.</p>
-
-		<p class={"text-xl my-4"}>En mi tiempo libre, disfruto pasar tiempo con mi familia, y también me gusta salir a caminar y explorar la naturaleza. Como desarrollador, me enfoco en aprender continuamente y mejorar mis habilidades, y estoy emocionado por las oportunidades que el futuro pueda traer.</p>
+      <div class={""}>
+      	<h1 class={"text-6xl font-bold my-8"}>{
+          lan.value==="es" ?  "Acerca de mi":"About me"
+        }</h1>
+        <div >
+          {
+            lan.value==="es" 
+              ? introduccionES.map((p,i)=><p dangerouslySetInnerHTML={p} class={"text-xl sm:mr-8 my-8"} key={i}> </p>)
+              : introductionEn.map((p,i)=><p dangerouslySetInnerHTML={p} class={"text-xl sm:mr-8 my-8"} key={i}> </p>)
+          }
+        </div>
+      </div>
+      <img src="/about_page.jpg" alt="" class={"w-[350px] h-[460px]"}/>
     </article>
   );
 });
