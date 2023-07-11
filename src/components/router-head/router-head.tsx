ae5e0e73ab49d1208a694eb3027daf7f0b802f39
Ander Carrasco <anderfrankdev@gmail.com>
2023-07-11 17:08:58 -0400

    aboufavicon added

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -13,9 +13,60 @@ export const RouterHead = component$(() => {
       <title>{head.title}</title>
 
       <link rel="canonical" href={loc.url.href} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, height=device-height, viewport-fit=cover"
+      />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />      
+      <link rel="icon" type="image/svg+xml" href="/logo_light.png" />
+    <meta name="theme-color" content="#000" />
 
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+
+    <link rel="apple-touch-icon" href="img/icons/icon-192x192.png" />
+    <link
+      rel="apple-touch-icon"
+      sizes="152x152"
+      href="img/icons/icon-152x152.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="img/icons/icon-192x192.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="167x167"
+      href="img/icons/icon-152x152.png"
+    />
+
+    <link
+      rel="apple-touch-startup-image"
+      media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+      href="img/icons-ios/apple-launch-1125x2436.png"
+    />
+    <link
+      rel="apple-touch-startup-image"
+      media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+      href="img/icons-ios/apple-launch-750x1334.png"
+    />
+    <link
+      rel="apple-touch-startup-image"
+      media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+      href="img/icons-ios/apple-launch-1242x2208.png"
+    />
+    <link
+      rel="apple-touch-startup-image"
+      media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+      href="img/icons-ios/apple-launch-640x1136.png"
+    />
+
+    <meta
+      name="apple-mobile-web-app-status-bar-style"
+      content="black-translucent"
+    />
+
+    <meta name="apple-mobile-web-app-title" content="Ander's portfolio" />
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
